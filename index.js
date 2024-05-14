6f5d0692f8961d52d2104680a10a6c83fb3b47b5
hientutitii <cateconnerpun0suy@hotmail.com>
2024-05-14 09:38:18 +0000

    add more features

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function factorial(n) {
-  if (n === 0 || n === 1) return 1;
-  return n * factorial(n - 1);
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
+  }
+  return [...map.values()];
 }
